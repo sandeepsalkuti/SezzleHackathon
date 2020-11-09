60623d01289af7b87e0c08861e3a7d4c1d589c21
sandeepsalkuti <salkutisandeep222@gmail.com>
2020-11-08 23:47:17 -0600

    second commit

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
+var port= process.env.PORT || 3000;
 var app = express();
-var server = app.listen(3000);
+var server = app.listen(port);
 app.use(express.static("public"));
 
 var socket = require("socket.io");
